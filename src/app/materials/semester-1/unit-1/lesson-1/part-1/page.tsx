14a28beb25e19edb66e891ab8eca33b3d52c4eea
Hazim Jaber <h75jbr@gmail.com>
2025-08-11 14:34:57 +0000

    اعتمد تسمية الازرار هذه في جميع اجزاء الدرس الاول القادمة

--- src/app/materials/semester-1/unit-1/lesson-1/part-1/page.tsx
+++ src/app/materials/semester-1/unit-1/lesson-1/part-1/page.tsx
@@ -305,7 +305,7 @@ export default function LessonPartPage() {
         <div className="flex justify-end">
           <Link href="/materials/semester-1/unit-1/lesson-1/part-2" passHref>
             <Button size="lg">
-              الجزء التالي: مقدمة قوانين الغازات
+              الجزء التالي
               <ArrowLeft className="mr-2 h-5 w-5" />
             </Button>
           </Link>
